fc895798a9a390be0e825cab63f8d28645c484fe
Edward Cheadle <echeadle@gmail.com>
2025-06-26 10:58:03 -0600

    Commiting to avoid another disaster

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from lg-quickstart!")
+import os
+from typing import Annotated
+
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
 
-if __name__ == "__main__":
-    main()
+graph_builder = StateGraph(State)
+
+
+llm = init_chat_model(openai_api_key=openai_api_key, model_name="gpt-4o.mini")
+
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph = graph_builder.compile()
+
+
+
+
+
+
