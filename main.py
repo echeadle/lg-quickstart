1285984002a3529e6e27967b55f0469453126adb
Edward Cheadle <echeadle@gmail.com>
2025-06-26 12:19:00 -0600

    Added ability to create image maps and finished Chatbot.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from IPython.display import Image, display
 import os
+import io
+from PIL import Image
 from typing import Annotated
 
 from langchain.chat_models import init_chat_model
@@ -9,9 +12,9 @@ from langgraph.graph.message import add_messages
 
 from dotenv import load_dotenv
 
+# The function load_dotenv() loads enviroment varibles
 load_dotenv()
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -19,12 +22,28 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
-
-llm = init_chat_model(openai_api_key=openai_api_key, model_name="gpt-4o.mini")
+# The init_chat_model, seems very basic, it does not take a key 
+# as an argument.
+llm = init_chat_model(model="gpt-4o-mini")
 
 
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
+
+def draw_graph(graph):
+    try:
+        img_data = graph.get_graph().draw_mermaid_png()
+        image = Image.open(io.BytesIO(img_data))
+        image.show()
+    except Exception:
+        # This requires some extra dependencies and is optional
+            pass
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
 
 
 # The first argument is the unique node name
@@ -35,7 +54,26 @@ graph_builder.add_edge(START, "chatbot")
 graph = graph_builder.compile()
 
 
+def main():
+    
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+            elif user_input.lower() in ["draw", "d"]:
+                draw_graph(graph)
+                break
+            stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
 
-
+if __name__ == "__main__":
+    main()
 
 
